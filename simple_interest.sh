9174c5c1b90dd927861ce25fe908495f80fb27d3
programmarself <134633289+programmarself@users.noreply.github.com>
2023-07-30 21:13:22 +0500

    Create simple_interest.sh

---
+++ simple_interest.sh
@@ -0,0 +1,13 @@
+GitHub
+Bitbucket
+GitLab
+CodePen
+Pastebin
+These services all offer free plans that allow you to host your script files. Once you've chosen a hosting service, you can upload your script file to the service and share the URL with your peers.
+
+Here are some additional tips for hosting a script file:
+
+Choose a hosting service that offers a free plan.
+Make sure the hosting service allows you to share the URL of your script file.
+Give your script file a descriptive name.
+Include a brief description of your script file in the hosting service's description field
